b56b8396b08f53907d527db7b7a27d314af3ec1f
BruneiSultana <rosecay.ro@gmail.com>
2023-08-16 05:40:23 -0700

    printx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * prints lowercase alphabet on 10 different lines
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = '0';
+
+	char letter = 'a';
+
+	while (count <= 10)
+	{
+		while(letter = 'z')
+		{
+			_putchar(letter);
+			letter++;
+		
+		}
+	
+		_putchar('\n');
+	}
+
+}
