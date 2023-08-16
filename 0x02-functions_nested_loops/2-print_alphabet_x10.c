39011b8fb1b6fa48ca6212b56e4b8302c93d4cf4
BruneiSultana <rosecay.ro@gmail.com>
2023-08-16 06:37:40 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,19 @@
 void print_alphabet_x10(void)
 {
 	int count = '0';
-	char letter;
 
-	while (count++ <= 9)
+	while (count < 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
 			_putchar(letter);
+			letter++;
+		}
+
 		_putchar('\n');
 	}
 
+	count++;
 }
