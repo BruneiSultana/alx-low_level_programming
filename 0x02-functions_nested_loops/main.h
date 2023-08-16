eab9a3d8820ec6bc44820cff5c64b67b74c58f0f
Rosemond O <rosecay.ro@gmail.com>
2023-08-15 19:23:35 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _main.h_
+#define _main.h_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
