49b2ccc30680245aea21a083912788682243b52a
BruneiSultana <rosecay.ro@gmail.com>
2024-01-19 09:59:52 -0800

    qq

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * main - dereferencing pointers, example with int and char types
- *
  * _puts_recursion - recursively prints a string, followed by a new line
- * @s - the strings to be printed
- *
+ * @s: the strings to be printed
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*s);
-		_puts_recursion(s + 1)
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	printf('\n');
+	_putchar('\n');
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,5 +18,14 @@ void _puts_recursion(char *s);
  *
  */
 
+int _putchar(char c);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success, 1
+ * on error, -1 is returned, and error is set appropriately.
+ */
 
 #endif
